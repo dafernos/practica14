5d5d223490dfcc30a223d1de45265ccee070a85b
Dario Fernandez <drfernos@gmail.com>
2018-02-22 02:15:31 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,26 +4,41 @@ $(document).ready(function($) {
         $('#his').toggle();
         $('#conprog').toggle('fade');
         $('#consys').toggle();
-        $('#formulario').toggle();
-        if ( $(this).css('left') === ($(window).width()*85/100)+"px") {
+        $('#formulario').toggle('fade');
+        if ( $(this).css('right') === ($(window).width()*5/100)+"px") {
             $(this).css('left','5vw');
-        } else {
-            $(this).css('left','85vw');
+            $(this).css('right','auto');
+            $(this).removeClass('botond').addClass('botoni');
+        }else {
+            $(this).css('left','auto');
+            $(this).css('right','5vw');
+            $(this).removeClass('botoni').addClass('botond');
         }
         $('#redes').toggle('fade');
-        
+        console.log($(window).width());
     });
     $('#botonExp').on('click',function(){
     	$('#boton').hide();
     });
+    if($(window).width()<(1024)){
+        $('#botonSM').on('click',function(){
+            $('#boton').show().fadeIn();
+        })
+        $('#botonCon').on('click',function(){
+            $('#boton').show();
+        })
+        $('#botonCont').on('click',function(){
+            $('#boton').show().fadeIn('slow');
+        });
+    }
+    $('a[href^="#"]').on('click', function(event) {
+      var target = $(this.getAttribute('href'));
+      if( target.length ) {
+          event.preventDefault();
+          $('html, body').stop().animate({
+              scrollTop: target.offset().top
+          }, 1000);
+      }
 
-$('#botonSM').on('click',function(){
-    $('#boton').show();
-})
-$('#botonCon').on('click',function(){
-    $('#boton').show();
-})
-$('#botonCont').on('click',function(){
-    $('#boton').show();
-})
+  });
 });
